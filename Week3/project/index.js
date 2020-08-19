2ba477bac57f52189e66662b060cf0e88c3d85cc
ismaiel <ismaiel.nab@gmail.com>
2020-08-19 19:43:12 +0200

    complete the project

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,3 +1,37 @@
 // Your code goes in here
 
-document.querySelector("#app").innerText = "Tip Calculator";
+// declare all the required variables//
+const tip = document.getElementById('tip');
+const bill = document.getElementById('bill');
+const service = document.getElementById('service');
+const sharing = document.getElementById('sharing');
+const calculate = document.getElementById('calculate');
+const amount = document.getElementById('amount');
+const people = document.getElementById('people');
+
+// do the tip calculation by click the calculate function//
+calculate.addEventListener('click', calculateTheTip);
+
+//to keep the number of sharing people one or more//
+sharing.addEventListener('change', changeTheLabel);
+
+function calculateTheTip() {
+  if (bill.value == ' ' || service.value == 0) {
+    alert('Please fill in all the fields');
+  } else {
+    let tipAmount = (bill.value * service.value) / sharing.value;
+    amount.innerText = tipAmount.toFixed(2);
+    tip.style.display = 'block';
+  }
+}
+function changeTheLabel() {
+  if (sharing.value <= 0) {
+    alert('Please enter a valid number');
+    sharing.value = 1;
+  }
+  if (sharing.value == 1) {
+    people.innerText = 'person';
+  } else {
+    people.innerText = 'people';
+  }
+}
